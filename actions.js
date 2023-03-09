70637cdc1dc454de32ed4b0b9b925af8156e371b
dn4lele <danielmaier4312@gmail.com>
2023-03-09 10:47:35 +0200

    lesson3

---
+++ actions.js
@@ -0,0 +1,54 @@
+import  express  from "express";
+const router=express.Router();
+
+
+
+router.get('/sayhello',(req,res)=>{
+    return res.status(200).json({
+        message:'hello from my api'
+    })
+
+})
+
+router.get('/getParam/:number/:tax',(req,res)=>{
+
+    const cost=req.params.number;
+    const taxes=req.params.tax;
+
+    return res.status(200).json({
+        message:cost*taxes
+    })
+
+})
+
+
+router.post('/calcSal',(req,res)=>{
+
+    //option 1
+    /*
+    const workingHours=req.body.workingHours;
+    const payPerHour=req.body.payPerHour;
+    const payPerExtraHour=req.body.payPerExtraHour;
+    const tax=req.body.tax;
+    */
+
+    //option 2
+    const{workingHours,payPerHour,payPerExtraHour,tax}=req.body;
+    let pay=0;
+    workingHours=parseInt(workingHours);
+    payPerHour=parseInt(payPerHour);
+    payPerExtraHour=parseInt(payPerExtraHour);
+    tax=parseInt(tax);
+
+    if(workingHours>180)
+
+
+    return res.status(200).json({
+        message:`hello ${workingHours} ${payPerHour} ${payPerExtraHour} ${tax}  \n 180 must and more is extra`
+        
+    })
+
+})
+
+
+export default router;
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from 'dotenv';
+import actions from './actions.js';
 dotenv.config();
 
 const app = express();
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 const port=process.env.PORT;
 
-app.listen(port,()=>{console.log(`the server is runing on port ${port}`)});
+//ROUTES
+app.use('/api',actions);
+
+
+
+
+app.listen(port,()=>{console.log(`the server is running via port ${port}`)});
 
 
 
 
 
-
-
-
-console.log("you ave nice")
